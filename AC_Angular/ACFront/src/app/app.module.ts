87c445789b3d800985ac8e3bfa2f28b089ad0ea8
John Riley <john203@revature.net>
2023-03-20 13:51:40 -0500

    adding signup

--- AC_Angular/ACFront/src/app/app.module.ts
+++ AC_Angular/ACFront/src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ AC_Angular/ACFront/src/app/models2/users2.ts
@@ -0,0 +1,8 @@
+export interface User2 {
+    id? : number,
+    fname : string, 
+    lname: string,
+    username: string,
+    password: string,
+    wallet: number
+}
