dd5172a7067c562563c0af957663bf6d5b653b9d
Eric Gibson <46939607+giberic4@users.noreply.github.com>
2023-03-21 12:28:01 -0400

    Just a start, sign-in, and services

--- AC_Angular/ACFront/src/app/sign-in/sign-in.component.spec.ts
+++ AC_Angular/ACFront/src/app/sign-in/sign-in.component.spec.ts
@@ -1,14 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { SignInComponent } from './sign-in.component';
+import { FormGroup, FormControl, FormBuilder, FormsModule } from '@angular/forms';
 
+
+//differences between comp and service test
+//they have lifecycle hooks, 
+//
 describe('SignInComponent', () => {
   let component: SignInComponent;
   let fixture: ComponentFixture<SignInComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SignInComponent ]
+      declarations: [ SignInComponent ],
+      imports: [
+        HttpClientTestingModule,
+        FormsModule
+      ]
     })
     .compileComponents();
 
@@ -20,4 +29,37 @@ describe('SignInComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should have a default user with empty values', () => {
+  //   expect(component.newuser).toEqual(
+  //     {
+  //       username :  "",
+  //       password : "",
+  //       fname : "",
+  //       lname : "",
+  //       wallet : 0
+  //     }
+  //   );
+  //   it('should expect username and password to change when calling login()', () => {
+  //     component.login('bobuser', 'passbob');
+      
+  //     expect(component.newuser).toBe(
+  //       {
+  //         username :  "bobuser",
+  //         password : "passbob",
+  //         fname : "",
+  //         lname : "",
+  //         wallet : 0
+  //       }
+  //     )
+
+
+
+
+  //   })
+
+
+
+  // });
+
 });
